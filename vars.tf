cadbcee3b95609956d9e9e636780a39e04b6238b
Mallika <kmallika.malli@gmail.com>
2023-10-12 18:11:03 +0530

    added rabbit mq DB

---
+++ vars.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  default = "rabbitmq"
+}
+variable "env" {}
+variable "allow_db_cidr" {}
+variable "vpc_id" {}
+variable "subnets" {}
+variable "tags" {}
+variable "port_no" {
+  default = "5672"
+}
+variable "kms_arn" {}
+variable "instance_type" {}
+variable "bastion_cidr_block" {}
